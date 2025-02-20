fbd51ce1179f0acdf9eb16397dfbfe95ad77633d
Sourav KV <spexod@gmail.com>
2025-02-20 20:47:29 +0530

    v7

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
+import { Button } from "@heroui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className=" bg-zinc-800 text-white  h-screen ">
       
-      <div className="  text-9xl p-5 flex justify-center pb-[200px]">BLOCKAID</div>
+      <div className="  bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent  text-9xl p-5 flex justify-center pb-[200px]">BLOCKAID</div>
       <div className=" flex justify-center">
-        <button className=" bg-zinc-400 rounded-xl p-4 flex justify-center">CREATE</button>
+      <Button
+      className="bg-gradient-to-tr from-pink-500 to-yellow-500 rounded-2xl p-8  hover:opacity-70 duration-300 text-white shadow-lg"
+      radius="full" disableRipple
+    >
+      Create
+    </Button>
       </div>
       <div>
         <div></div>
@@ -15,3 +21,5 @@ export default function Home() {
     </div>
   );
 }
+
+
