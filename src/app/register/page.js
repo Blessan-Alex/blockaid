8c96b6309b2f7b39f085c70633885c0a48bdb40e
Aswin <aswink@lpu.in>
2025-02-20 19:28:31 +0530

    added register, motion, shadcn

---
+++ src/app/register/page.js
@@ -0,0 +1,10 @@
+export default function Register() {
+  return (
+    <div className="h-screen w-full grid grid-cols-2 grid-rows-1">
+      <div className="bg-slate-600 flex items-center justify-center p-8">
+        <form action=""></form>
+      </div>
+      <div className="bg-orange-400"></div>
+    </div>
+  );
+}
