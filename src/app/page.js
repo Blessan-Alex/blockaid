38c0f258ab76a132c70c0d4fbabd942951ff5fe1
Sourav KV <spexod@gmail.com>
2025-02-20 20:02:24 +0530

    newww

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" bg-zinc-800 text-white">
-      hy
-      <div className="  text-9xl p-5">BLOCKAID</div>
-      <div>
-        <button className=" bg-zinc-400 rounded-lg p-4">CREATE</button>
+    <div className=" bg-zinc-800 text-white  h-screen ">
+      
+      <div className="  text-9xl p-5 flex justify-center pb-[200px]">BLOCKAID</div>
+      <div className=" flex justify-center">
+        <button className=" bg-zinc-400 rounded-xl p-4 flex justify-center">CREATE</button>
       </div>
       <div>
         <div></div>
-        <input></input>
+        
       </div>
     </div>
   );
