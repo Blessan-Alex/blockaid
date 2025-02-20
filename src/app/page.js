dbad0975b2cf1e5b664fa239d884b50100d00e1d
Aswin <aswink@lpu.in>
2025-02-20 18:11:36 +0530

    add commit

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={40}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -18,7 +18,6 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.js
             </code>
-            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
